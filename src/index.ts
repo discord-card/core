74b0a90fb2ea576b35dcd13d9573914c3068ccea
AKORA <dragoncatm@gmail.com>
2022-04-12 18:51:27 +0200

    :clock1: Create Timer

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import './types';
 
 export * from './types';
 export * from './util/image';
-export * from './util/text';
 export * from './util/lib';
+export * from './util/text';
+export * from './util/timer';
 export * from './gradient';
---
+++ src/util/timer.ts
@@ -0,0 +1,36 @@
+export class Timer {
+  public name: string;
+  public startTime: Date;
+  private lastTime: Date;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public start() {
+    this.startTime = new Date();
+    this.lastTime = new Date();
+    return this;
+  }
+
+  public step(): this;
+  public step(name?: string) {
+    const diff = this.diff();
+    if (name) {
+      console.log(this.name, '-', name, ':', diff, 'ms');
+    } else {
+      console.log(this.name, ':', diff, 'ms');
+    }
+    this.lastTime = new Date();
+    return this;
+  }
+
+  public stop() {
+    const complete = new Date().getTime() - this.startTime.getTime();
+    console.log(this.name, 'took', ':', complete, 'ms');
+  }
+
+  private diff() {
+    return new Date().getTime() - this.lastTime.getTime();
+  }
+}
